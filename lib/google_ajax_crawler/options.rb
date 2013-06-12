aa5ef45d97e0624371392fed020d6e18625bb3eb
Christophe <christophe.belpaire@gmail.com>
2013-06-12 13:11:41 +0200

    fix dependencies

--- lib/google_ajax_crawler/options.rb
+++ lib/google_ajax_crawler/options.rb
@@ -4,7 +4,7 @@ module GoogleAjaxCrawler
 
     def initialize(app, &block)
       @driver  = Drivers::Poltergeist.new(self)
-      @timeout = 30
+      @timeout = 90
       @requested_route_key  = '_escaped_fragment_'
       @response_headers     = { 'Content-Type' => 'text/html' }
       @poll_interval        = 0.5
